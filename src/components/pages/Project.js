1a672d8638382b497452cdb45a13f9cd5d78a6f7
brait <lorimiervb@gmail.com>
2024-07-30 18:52:05 -0300

    add: class 33

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project(){
     const {id} = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState([])
     const [type, setType] = useState([])
 
@@ -34,7 +35,14 @@ function Project(){
         setShowProjectForm(!showProjectForm)
     }
 
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
+    }
+
     function editPost(project){
+
+        setMessage('')
+
         if(project.budget < project.cost){
             setMessage('O orçamento não pode ser menor que o custo do projeto')
             setType('error')
@@ -85,6 +93,17 @@ function Project(){
                         </div>
                     )}
                 </div>
+                <div className={styles.service_form_container}>
+                    <h2>Adicione um servico: </h2>
+                    <button className={styles.btn} onClick={toggleServiceForm}>{!showServiceForm ? 'Adicionar serviço' : 'Fechar'}</button>
+                    <div className={styles.project_info}>
+                        {showServiceForm && <div>formulário do serviço</div>}
+                    </div>
+                </div>
+                <h2>Serviços</h2>
+                <Container customClass="start">
+                    <p>Itens de serviços</p>
+                </Container>
             </Container>
         </div>
        ) : (
