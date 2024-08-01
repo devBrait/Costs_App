4f03d090870a8541a8180c4434b71d642f4e8d3a
brait <lorimiervb@gmail.com>
2024-08-01 18:38:08 -0300

    final version

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -10,6 +10,7 @@ import Container from '../layout/Container'
 import ProjectForm from '../project/ProjectForm'
 import Message from '../layout/Message'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard' 
 
 function Project(){
 
@@ -17,6 +18,7 @@ function Project(){
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [showServiceForm, setShowServiceForm] = useState(false)
+    const [services, setServices] = useState([])
     const [message, setMessage] = useState([])
     const [type, setType] = useState([])
 
@@ -30,10 +32,35 @@ function Project(){
             }).then(resp => resp.json())
                 .then((data) => {
                     setProject(data)
+                    setServices(data.services)
                 })
         }, 300)
     }, [id])
 
+
+    function removeService(id, cost){
+        const servicesUpdate = project.services.filter(
+            (service) => service.id !== id
+        )
+        const projectUpdate = project
+
+        projectUpdate.services = servicesUpdate
+        projectUpdate.cost = parseFloat(project.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdate.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdate)
+        }).then((resp) => resp.json())
+          .then((data) => {
+            setProject(projectUpdate)
+            setServices(servicesUpdate)
+            setMessage('Serviço removido com sucesso')
+          })
+          .catch(err => console.log(err))
+    }
 
     function createService(project){
         setMessage('')
@@ -62,7 +89,7 @@ function Project(){
             body: JSON.stringify(project)
         }).then((resp) => resp.json())
           .then((data) => {
-            console.log(data)
+            setShowServiceForm(false)
           })
           .catch(err => console.log(err))
     }
@@ -140,7 +167,12 @@ function Project(){
                 </div>
                 <h2>Serviços</h2>
                 <Container customClass="start">
-                    <p>Itens de serviços</p>
+                    {services.length > 0 &&
+                        services.map((service) => (
+                            <ServiceCard id={service.id} name={service.name} cost={service.cost} description={service.description} key={service.id} handleRemove={removeService}/>
+                        ))
+                    }
+                    {services.length == 0 && <p>Não há serviços cadastrados</p>}
                 </Container>
             </Container>
         </div>
